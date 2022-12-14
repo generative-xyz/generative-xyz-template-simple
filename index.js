5381d885686f1effd97260f328949bd04c5873bc
thaibao-incognito <thaibao@autonomous.nyc>
2022-12-14 09:20:33 +0700

    - init template simple

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Your code here
+    let containerDiv = document.getElementById("container-el");
+    containerDiv.innerText = "Hello";
+});
