64ce5cfa501f948e18d47b9e75cdee80e31dd27e
thaibao-incognito <thaibao@autonomous.nyc>
2022-12-14 13:57:02 +0700

    - traits for token

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const rand = mathRand();
+
+    // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ Your traits here ↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+    function T1(rand) {
+        // calculate rarity by yourself
+        if (rand > 0.5) {
+            return true;
+        } else {
+            return false
+        }
+    }
+
+    // window.$generativeTraits contain list traits for token
+    // you need to populate a $generativeTraits object in the window object
+    window.$generativeTraits = {
+        "T1": T1(rand),
+    }
+    console.log(window.$generativeTraits)
+    // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ Your traits here ↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+
     // ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ Your code here ↓↓↓↓↓↓↓↓↓↓↓↓↓↓
     let containerDiv = document.getElementById("container-el");
     containerDiv.innerHTML = "<span>tokenId: " + tokenId + "</span><br>" + "<span>tokenMintNumber: " + tokenMintNumber + "</span><br>" + "<span>project: " + projectNumber + "</span><br>" + "<span>seed: " + seed + "</span><br>";
+
     // ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ Your code here ↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 });
